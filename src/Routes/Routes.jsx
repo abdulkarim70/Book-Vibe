d700a07fd62c6b35e49c48b45bffc1a37e68643a
abdulkarim70 <mmd.abdulkarim@gmail.com>
2026-04-06 17:20:05 +0600

    Book Vibe

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,29 @@
+import { createBrowserRouter } from "react-router";
+
+import MainLayout from "../Layout/MainLayout";
+import Books from "../Pages/Books/Books";
+import Homepage from "../Pages/Hompage/Homepage";
+
+export const router =createBrowserRouter([{
+  path:'/',
+  Component:MainLayout,
+  children:[{
+    index:true,
+    element:<Homepage/>
+  },
+  {
+    path:'/books',
+    element:<Books/>
+  }
+
+
+
+  ]
+},
+
+
+
+
+]
+
+)
